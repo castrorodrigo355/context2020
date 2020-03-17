554260bbbb6c1369aac17885ad6fe84f8d5640ed
castrorodrigo355 <castrorodrigo355@gmail.com>
2020-03-17 15:43:23 -0300

    117

--- src/components/modalProfile/modalProfile.js
+++ src/components/modalProfile/modalProfile.js
@@ -1,45 +1,30 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import Dialog from '@material-ui/core/Dialog';
+import DvrIcon from '@material-ui/icons/Dvr';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import "./modalProfile.css";
-
-const useStyles = makeStyles(theme => ({
-    modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    paper: {
-        backgroundColor: theme.palette.background.paper,
-        border: '1px solid #000',
-        padding: theme.spacing(2, 4, 3),
-        height: "200px"
-    },
-}));
 
 const ModalProfile = ({openModalProfile, handleClose, profileMessage}) => {
 
-    const classes = useStyles();
 
     const { title, subtitle, topics } = profileMessage;
 
     return (
-        <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            className={classes.modal}
-            open={openModalProfile}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{timeout: 500}}
-        >
-            <Fade in={openModalProfile}>
-                <div className={classes.paper}>
-                    <h2 id="transition-modal-title">{`Presentación: ${title}`}</h2>
-                    <p id="transition-modal-description">{`Cursos: ${subtitle} Horas`}</p>
+        <Dialog open={openModalProfile}
+                onClose={handleClose}>
+            <div className="modal-profile-container">
+                <div className="modal-profile-header">
+                    {title}
+                    <IconButton aria-label="close" color="inherit" onClick={handleClose}>
+                        <CloseIcon/>
+                    </IconButton>
+                </div>
+                <div className="modal-profile-subtitle">
+                    {`${subtitle} Horas`}
+                </div>
+                <hr/>
+                <div className="modal-profile-content">
                     {
                         topics.map((t, i) => {
                             return(
@@ -50,8 +35,11 @@ const ModalProfile = ({openModalProfile, handleClose, profileMessage}) => {
                         })
                     }
                 </div>
-            </Fade>
-        </Modal>
+                <div className="modal-profile-footer">
+                    { <DvrIcon fontSize="large"/> }
+                </div>
+            </div>
+        </Dialog>
     )
 }
 
